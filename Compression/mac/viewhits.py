6c2cfc14d538393b4abfb53339835805d9437d3a
David <devish.c@gmail.com>
2015-05-16 16:11:13 -0500

    updating hit maker/viewer python script to be compatible w/ larlite --David Caratelli

--- Compression/mac/viewhits.py
+++ Compression/mac/viewhits.py
@@ -1,32 +1,27 @@
 # Load libraries
-import ROOT, sys, os
+import sys, os
 from ROOT import *
+from ROOT import gSystem
+from ROOT import larlite as fmwk
+from ROOT import compress
 
-# Now import ana_processor & your class. For this example, ana_base.
-gSystem.Load("libAnalysis")
-gSystem.Load("libLArUtil")
-gSystem.Load("libSNCompression")
+# Create ana_processor instance
+my_proc=fmwk.ana_processor()
 
-filename = sys.argv[1]
+# Specify IO mode
+#my_proc.set_io_mode(fmwk.storage_manager.kBOTH)
+#my_proc.set_io_mode(fmwk.storage_manager.kWRITE)
+my_proc.set_io_mode(fmwk.storage_manager.kREAD)
 
-my_proc = larlight.ana_processor()
 
-my_proc.set_verbosity(larlight.MSG.DEBUG)
+for x in xrange(len(sys.argv)-1):
+    my_proc.add_input_file(sys.argv[x+1])
 
-my_proc.set_io_mode(larlight.storage_manager.READ)
-
-#my_proc.set_input_rootdir("scanner")
-
-my_proc.add_input_file(filename)
-
-larlight.storage_manager.get().set_in_rootdir("scanner")
-larlight.storage_manager.get().set_data_to_read(larlight.DATA.FFTHit)
-larlight.storage_manager.get().set_data_to_read(larlight.DATA.UserInfo,False)
 
 #is there a way to disable ana_proc from creating an output file at all?
 my_proc.set_ana_output_file("")
 
-my_ana = larlight.HitViewer()
+my_ana = fmwk.HitViewer()
 
 my_proc.add_process(my_ana)
 
